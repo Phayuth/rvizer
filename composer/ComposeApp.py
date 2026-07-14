871f03eb0fa7231854a494e57d514122dc7127f9
yuth <yuth@todo.com>
2026-07-14 23:43:04 +0900

    yy

--- composer/ComposeApp.py
+++ composer/ComposeApp.py
@@ -3,22 +3,15 @@ import viser
 import viser.transforms as tf
 import yourdfpy
 import time
-import threading
 import yaml
 from robot_descriptions.loaders.yourdfpy import load_robot_description
 from bubblify.core import EnhancedViserUrdf
-from rvizer.osutils import os_select_folder, os_list_directory
-from pathlib import Path
 
 
 class ComposeApp:
 
     def __init__(self, port=8080):
-        self.server = viser.ViserServer(port=port)
-
-        # urdf_path = Path("/rb3_730es_u/rb3_730es_u_armright.urdf")
-        # urdf_path = Path("/humanoid_urdf/lift.urdf")
-        # urdf_path = Path("/humanoid_urdf/humanoid_urdf.urdf")
+        self.srv = viser.ViserServer(port=port)
 
         show_collision = True
         # Load URDF
@@ -29,7 +22,7 @@ class ComposeApp:
         #     build_collision_scene_graph=show_collision,
         #     load_collision_meshes=show_collision,
         # )
-        robot_name = "robotiq_2f85"
+        robot_name = "panda"
         self.urdf = load_robot_description(
             robot_name + "_description",
             load_meshes=True,
@@ -40,7 +33,7 @@ class ComposeApp:
 
         # Enhanced URDF visualizer with per-link control
         self.urdf_viz = EnhancedViserUrdf(
-            self.server,
+            self.srv,
             urdf_or_path=self.urdf,
             load_meshes=True,
             load_collision_meshes=show_collision,
@@ -49,15 +42,38 @@ class ComposeApp:
         # self.urdf.base_link
 
         self.joint_sliders = []
+        self._setup_compose_config()
         self._setup_robot_controls()
+        self._setup_tf()
         self._setup_stack()
+
+    def _setup_compose_config(self):
+        self.srv.scene.world_axes.visible = True
+        self.srv.scene.world_axes.axes_length = 5
+        self.srv.scene.world_axes.axes_radius = 0.001
+        self.srv.gui.configure_theme(
+            control_width="large",
+            control_layout="floating",
+        )
+        self.srv.scene.add_grid(
+            "/reference_grid",
+            width=10,
+            height=10,
+            position=(0.0, 0.0, 0.0),
+            cell_color=(200, 200, 200),
+            cell_thickness=1.0,
+        )
+
+        with self.srv.gui.add_folder("RVizer Config", expand_by_default=False):
+            self.srv.gui.add_button_group(
+                label="Save/Load", options=("Load", "Save")
+            )
 
     def _setup_robot_controls(self):
         """Setup robot configuration and visibility controls."""
-        with self.server.gui.add_folder("🤖 Robot Controls"):
+        with self.srv.gui.add_folder("🤖 Robot Controls"):
             # Joint sliders
             initial_config = []
-
             for joint_name, (
                 lower,
                 upper,
@@ -68,7 +84,7 @@ class ComposeApp:
                     0.0 if lower < -0.1 and upper > 0.1 else (lower + upper) / 2.0
                 )
 
-                slider = self.server.gui.add_slider(
+                slider = self.srv.gui.add_slider(
                     label=joint_name,
                     min=lower,
                     max=upper,
@@ -78,10 +94,31 @@ class ComposeApp:
                 self.joint_sliders.append(slider)
                 initial_config.append(initial_pos)
 
+            # link
+            link_names = [link.name for link in self.urdf.robot.links]
+            link_handles = []
+            for i in range(len(link_names)):
+                l = self.srv.scene.add_frame(
+                    f"/link_{link_names[i]}_frame",
+                    position=(0, 0, 0),
+                    wxyz=(1, 0, 0, 0),
+                    scale=0.05,
+                )
+                link_handles.append(l)
+
             # Connect sliders to URDF update
             def update_robot_config():
                 config = np.array([s.value for s in self.joint_sliders])
                 self.urdf_viz.update_cfg(config)
+
+                for i, link_name in enumerate(link_names):
+                    T_link_world = self.urdf.get_transform(
+                        link_name, self.urdf.base_link, collision_geometry=True
+                    )
+                    position = T_link_world[:3, 3]
+                    wxyz = tf.SO3.from_matrix(T_link_world[:3, :3]).wxyz
+                    link_handles[i].position = position
+                    link_handles[i].wxyz = wxyz
 
             for slider in self.joint_sliders:
                 slider.on_update(lambda _: update_robot_config())
@@ -90,22 +127,74 @@ class ComposeApp:
             update_robot_config()
 
             # Reset button
-            reset_joints_btn = self.server.gui.add_button("🏠 Reset to Home")
+            reset_joints_btn = self.srv.gui.add_button("🏠 Reset to Home")
 
             @reset_joints_btn.on_click
             def _(_):
                 for slider, init_val in zip(self.joint_sliders, initial_config):
                     slider.value = init_val
 
+            for h in link_handles:
+
+                @h.on_click
+                def _(event: viser.GuiEvent):
+                    event.client.add_notification(
+                        f"Clicked on link: {h.name}",
+                        body=f"Position: {h.position}, Orientation (wxyz): {h.wxyz}",
+                        auto_close_seconds=3.0,
+                    )
+
+    def _setup_tf(self):
+        print("Links:")
+        link_names = [link.name for link in self.urdf.robot.links]
+        joint_names = [joint.name for joint in self.urdf.robot.joints]
+        joint_connections = [
+            (joint.name, joint.parent, joint.child)
+            for joint in self.urdf.robot.joints
+        ]
+
+        with self.srv.gui.add_folder("🧭 TF Viewer"):
+            dd_links = self.srv.gui.add_dropdown(
+                label="Links",
+                options=link_names,
+                initial_value=link_names[0],
+            )
+            self.srv.gui.add_divider()
+
+            dd_joints = self.srv.gui.add_dropdown(
+                label="Joints",
+                options=joint_names,
+                initial_value=joint_names[0],
+            )
+            t_pas = self.srv.gui.add_text(
+                "Parent Link",
+                initial_value=joint_connections[0][1],
+                disabled=True,
+            )
+            t_chs = self.srv.gui.add_text(
+                "Child Link",
+                initial_value=joint_connections[0][2],
+                disabled=True,
+            )
+
+            @dd_joints.on_update
+            def _(event: viser.GuiEvent):
+                t_pas.value = joint_connections[
+                    joint_names.index(dd_joints.value)
+                ][1]
+                t_chs.value = joint_connections[
+                    joint_names.index(dd_joints.value)
+                ][2]
+
     def _setup_stack(self):
         """Setup the GUI stack for the application."""
-        with self.server.gui.add_folder("📦 Stack") as folder_stack:
-            btn_load = self.server.gui.add_button("Load Environment Object")
+        with self.srv.gui.add_folder("📦 Stack") as folder_stack:
+            btn_load = self.srv.gui.add_button("Load Environment Object")
 
             @btn_load.on_click
             def _(event: viser.GuiEvent):
                 with folder_stack:
-                    bbbb = self.server.gui.add_button("Remove Environment Object")
+                    bbbb = self.srv.gui.add_button("Remove Environment Object")
 
                     @bbbb.on_click
                     def _(event: viser.GuiEvent):
--- composer/tools.py
+++ composer/tools.py
@@ -1,50 +1,92 @@
 import numpy as np
 import yourdfpy
-import time
-import threading
-import yaml
 from pathlib import Path
-import pprint
 import robot_descriptions
+from robot_descriptions.loaders.yourdfpy import load_robot_description
 
-# print(robot_descriptions.DESCRIPTIONS)
-urdf_path = Path("/rb3_730es_u/rb3_730es_u.urdf")
-# urdf_path = Path("/humanoid_urdf/lift.urdf")
-show_collision = True
 
-# Load URDF
-if urdf_path is not None:
+def download_robot_description(robot_name="panda"):
+    # print(robot_descriptions.DESCRIPTIONS) # list all available robot descriptions
+
+    show_collision = True
+    urdf = load_robot_description(
+        robot_name + "_description",
+        load_meshes=True,
+        build_scene_graph=True,
+        load_collision_meshes=show_collision,
+        build_collision_scene_graph=show_collision,
+    )
+
+    urdf.write_xml_file(str(Path(f"{robot_name}.urdf")))
+
+
+def append_prefix_to_urdf_links_joints(urdf_input, prefix, urdf_output):
+    show_collision = True
     urdf = yourdfpy.URDF.load(
-        str(urdf_path),  # urdf_path,
+        str(urdf_input),  # urdf_input,
         build_scene_graph=True,
         load_meshes=True,
         build_collision_scene_graph=show_collision,
         load_collision_meshes=show_collision,
     )
-    urdf_path = urdf_path
-urdf.show()
 
-print("URDF loaded successfully.")
-print(urdf.base_link)
-print(urdf.get_transform(urdf.base_link, urdf.base_link))
-print(urdf.robot.links)
-for link in urdf.robot.links:
-    pprint.pprint(
-        f"Link: {link.name}, Visuals: {len(link.visuals)}, Collisions: {len(link.collisions)}"
+    # rename links
+    for link in urdf.robot.links:
+        link.name = prefix + link.name
+
+    # rename joints
+    for joint in urdf.robot.joints:
+        joint.name = prefix + joint.name
+        joint.parent = prefix + joint.parent
+        joint.child = prefix + joint.child
+
+    # write the modified URDF to a new file
+    urdf.write_xml_file(str(urdf_output))
+
+
+def urdf_details(urdf_input):
+    show_collision = True
+    urdf = yourdfpy.URDF.load(
+        str(urdf_input),  # urdf_input,
+        build_scene_graph=True,
+        load_meshes=True,
+        build_collision_scene_graph=show_collision,
+        load_collision_meshes=show_collision,
     )
+    print(f"Base link: {urdf.base_link}")
+
+    print("Links:")
+    for link in urdf.robot.links:
+        print(f"  {link.name}")
+
+    print("\nJoints:")
+    for joint in urdf.robot.joints:
+        print(f"  {joint.name} (parent: {joint.parent}, child: {joint.child})")
 
 
-# 2. Define the prefix you want to add
-prefix = "armright_"
+if __name__ == "__main__":
+    # urdf_input = Path("/rb3_730es_u/rb3_730es_u.urdf")
+    # urdf_output = Path("/rb3_730es_u/rb3_730es_u_armright.urdf")
+    # prefix = "armright_"
+    # append_prefix_to_urdf_links_joints(urdf_input, prefix, urdf_output)
 
-# Rename links
-for link in urdf.robot.links:
-    link.name = prefix + link.name
+    show_collision = True
+    robot_name = "panda"
+    urdf = load_robot_description(
+        robot_name + "_description",
+        load_meshes=True,
+        build_scene_graph=True,
+        load_collision_meshes=show_collision,
+        build_collision_scene_graph=show_collision,
+    )
+    print(urdf.base_link)
+    print("Links:")
+    for link in urdf.robot.links:
+        print(f"  {link.name}")
 
-# Rename joints and their references
-for joint in urdf.robot.joints:
-    joint.name = prefix + joint.name
-    joint.parent = prefix + joint.parent
-    joint.child = prefix + joint.child
+    print("\nJoints:")
+    for joint in urdf.robot.joints:
+        print(f"  {joint.name} (parent: {joint.parent}, child: {joint.child})")
 
-urdf.write_xml_file("/rb3_730es_u/rb3_730es_u_armright.urdf")
+    TF = urdf.get_transform("panda_link0", "panda_hand_tcp")
+    print(f"==>> TF: \n{TF}")
