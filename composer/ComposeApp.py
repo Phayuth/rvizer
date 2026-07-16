276cb323ec77d2a65aabacb49be0354544c13af6
yuth <yuth@todo.com>
2026-07-16 17:22:46 +0900

    ii

--- composer/ComposeApp.py
+++ composer/ComposeApp.py
@@ -6,6 +6,7 @@ import time
 import yaml
 from robot_descriptions.loaders.yourdfpy import load_robot_description
 from bubblify.core import EnhancedViserUrdf
+from viser.extras import ViserUrdf
 
 
 class ComposeApp:
@@ -14,7 +15,8 @@ class ComposeApp:
         self.srv = viser.ViserServer(port=port)
 
         show_collision = True
-        # Load URDF
+        # urdf_path = "/humanoid_urdf.urdf"
+        # self.robot_name = "humanoid"
         # self.urdf = yourdfpy.URDF.load(
         #     str(urdf_path),  # urdf_path,
         #     build_scene_graph=True,
@@ -22,15 +24,16 @@ class ComposeApp:
         #     build_collision_scene_graph=show_collision,
         #     load_collision_meshes=show_collision,
         # )
-        robot_name = "panda"
+        self.robot_name = "panda"
         self.urdf = load_robot_description(
-            robot_name + "_description",
+            self.robot_name + "_description",
             load_meshes=True,
             build_scene_graph=True,
             load_collision_meshes=show_collision,
             build_collision_scene_graph=show_collision,
         )
 
+        self.root_node_name = "/moving_base"
         # Enhanced URDF visualizer with per-link control
         self.urdf_viz = EnhancedViserUrdf(
             self.srv,
@@ -38,6 +41,7 @@ class ComposeApp:
             load_meshes=True,
             load_collision_meshes=show_collision,
             collision_mesh_color_override=(1.0, 0.0, 0.0, 0.4),
+            root_node_name=self.root_node_name,
         )
         # self.urdf.base_link
 
@@ -99,32 +103,26 @@ class ComposeApp:
             link_handles = []
             for i in range(len(link_names)):
                 l = self.srv.scene.add_frame(
-                    f"/link_{link_names[i]}_frame",
+                    f"/{self.robot_name}/link_{link_names[i]}_frame",
                     position=(0, 0, 0),
                     wxyz=(1, 0, 0, 0),
                     scale=0.05,
                 )
                 link_handles.append(l)
 
-            # Connect sliders to URDF update
-            def update_robot_config():
-                config = np.array([s.value for s in self.joint_sliders])
-                self.urdf_viz.update_cfg(config)
+            # Connect sliders to URDF update worked
+            # def update_robot_config():
+            #     config = np.array([s.value for s in self.joint_sliders])
+            #     self.urdf_viz.update_cfg(config)
 
-                for i, link_name in enumerate(link_names):
-                    T_link_world = self.urdf.get_transform(
-                        link_name, self.urdf.base_link, collision_geometry=True
-                    )
-                    position = T_link_world[:3, 3]
-                    wxyz = tf.SO3.from_matrix(T_link_world[:3, :3]).wxyz
-                    link_handles[i].position = position
-                    link_handles[i].wxyz = wxyz
-
-            for slider in self.joint_sliders:
-                slider.on_update(lambda _: update_robot_config())
-
-            # Apply initial configuration
-            update_robot_config()
+            #     for i, link_name in enumerate(link_names):
+            #         T_link_world = self.urdf.get_transform(
+            #             link_name, self.urdf.base_link, collision_geometry=True
+            #         )
+            #         position = T_link_world[:3, 3]
+            #         wxyz = tf.SO3.from_matrix(T_link_world[:3, :3]).wxyz
+            #         link_handles[i].position = position
+            #         link_handles[i].wxyz = wxyz
 
             # Reset button
             reset_joints_btn = self.srv.gui.add_button("🏠 Reset to Home")
@@ -145,6 +143,61 @@ class ComposeApp:
 
                 h.on_click(handle_link_click)
 
+            btn_reset_base = self.srv.gui.add_button("🏠 Reset Base Position")
+            base = self.srv.scene.add_frame(
+                "/moving_base",
+                position=(0, 0, 0),
+                wxyz=(1, 0, 0, 0),
+            )
+            gizmo = self.srv.scene.add_transform_controls(
+                name="/gizmo",
+                scale=1.0,
+                active_axes=(True, True, False),
+            )
+
+            @gizmo.on_update
+            def _(event):
+                base.position = gizmo.position
+                base.wxyz = gizmo.wxyz
+                update_robot_config()
+
+            def update_robot_config():
+                config = np.array([s.value for s in self.joint_sliders])
+                self.urdf_viz.update_cfg(config)
+
+                for i, link_name in enumerate(link_names):
+                    T_link_base = self.urdf.get_transform(
+                        link_name, self.urdf.base_link, collision_geometry=True
+                    )
+
+                    base_to_world_position = base.position
+                    base_to_world_wxyz = base.wxyz
+                    base_to_world_xyzw = np.array(
+                        (
+                            base_to_world_wxyz[1],
+                            base_to_world_wxyz[2],
+                            base_to_world_wxyz[3],
+                            base_to_world_wxyz[0],
+                        )
+                    )
+                    base_to_world_matrix = tf.SE3.from_rotation_and_translation(
+                        rotation=tf.SO3.from_quaternion_xyzw(base_to_world_xyzw),
+                        translation=np.array(base_to_world_position),
+                    )
+
+                    T_link_world = base_to_world_matrix.as_matrix() @ T_link_base
+                    position_world = T_link_world[:3, 3]
+                    wxyz_world = tf.SO3.from_matrix(T_link_world[:3, :3]).wxyz
+
+                    link_handles[i].position = position_world
+                    link_handles[i].wxyz = wxyz_world
+
+            for slider in self.joint_sliders:
+                slider.on_update(lambda _: update_robot_config())
+
+            # Apply initial configuration
+            update_robot_config()
+
     def _setup_tf(self):
         print("Links:")
         link_names = [link.name for link in self.urdf.robot.links]
@@ -155,6 +208,11 @@ class ComposeApp:
         ]
 
         with self.srv.gui.add_folder("🧭 TF Viewer"):
+            t_base = self.srv.gui.add_text(
+                "Base Link", initial_value=self.urdf.base_link, disabled=True
+            )
+            self.srv.gui.add_divider()
+
             dd_links = self.srv.gui.add_dropdown(
                 label="Links",
                 options=link_names,
